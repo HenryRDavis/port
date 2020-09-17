fc28919b23d89b0bbd431ecf0af5b2cb5e68e25c
HenryRDavis <rainfallsprings@gmail.com>
2020-09-17 17:51:43 -0500

    hmmm

--- src/About.js
+++ src/About.js
@@ -6,8 +6,8 @@ const About = () => {
             <h1>What am I all about?</h1>
 
             <p>In all reality I'm a very simple person with very simple needs.</p>
-            <p>It often takes a while for me to be comfortable when meeting new people.</p>
-            
+            <p></p>
+
         </div>
     )
 }
